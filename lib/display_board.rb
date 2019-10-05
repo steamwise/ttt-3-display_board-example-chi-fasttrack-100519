48e81a613b4c9d1cfaf5865b6b89f30d45c3366c
puckish-optimization-7921 <r.blanchette47@gmail.com>
2019-10-05 20:42:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,8 @@
 
 def display_board(board)
 puts (" #{board[0]} | #{board[1]} | #{board[2]} ")   
-puts ("-----------") 
-puts ("   |   |   ")  
-puts ("-----------")  
-puts ("   |   |   ")  
+puts (" #{board[3]} | #{board[4]} | #{board[5]} ") 
+puts (" #{board[6]} | #{board[7]} | #{board[8]} ")   
 
 
 
