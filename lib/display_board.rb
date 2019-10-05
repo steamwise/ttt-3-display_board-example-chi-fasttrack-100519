6ddc2c8c97d48fe82d9486cd60898199612d2d49
puckish-optimization-7921 <r.blanchette47@gmail.com>
2019-10-05 20:32:40 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-def display_board
-puts ("   |   |   ")   
+def display_board(board)
+puts (" #{board[0]} | #{board[1]} | #{board[2]} ")   
 puts ("-----------") 
 puts ("   |   |   ")  
 puts ("-----------")  
